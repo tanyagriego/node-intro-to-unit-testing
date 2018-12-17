3d093d1ae93621f656944bd8323afefe76d4f2cc
tanyagriego <tanya.griego@gmail.com>
2018-12-16 17:55:02 -0800

    test

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,4 @@
+// Inside this file, write unit tests for the fizzBuzzer 
+//function found in fizzBuzzer.js. Be sure to test the normal case 
+//on a range of representative inputs, and test for at least one 
+//edge case (for instance, non-numeric inputs).
